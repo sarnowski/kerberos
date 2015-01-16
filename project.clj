bcdf904097f666a56b3fe2b27e64eb351e39ac7a
Tobias Sarnowski <tobias@sarnowski.io>
2015-01-16 17:07:46 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject kerberos "0.1.0-SNAPSHOT"
+  :description "A Clojure library for kerberos authentication."
+  :url "https://github.com/sarnowski/kerberos"
+
+  :license {:name "ISC"
+            :url "http://www.isc.org/downloads/software-support-policy/isc-license/"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
